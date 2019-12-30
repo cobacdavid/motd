1bbfa3e92d5df0d3ec6fbc354588895e7f3e583a
cobacdavid <7426305+cobacdavid@users.noreply.github.com>
2019-12-30 14:34:37 +0100

    first

---
+++ build/lib/motd/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from .motd import *
+
+
+version = '0.1'
+
+if __name__ == '__main__':
+    import argparse
+
+    parseur_args = argparse.ArgumentParser(
+        description="Mathematicians Of The Day")
+
+    parseur_args.add_argument('-d',
+                              '--date',
+                              help='JJ/MM')
+    parseur_args.add_argument('decalage',
+                              nargs='?',
+                              default='+0')
+
+    args = parseur_args.parse_args()
+
+    m = motd(args.date, args.decalage)
+    dico = json.loads(m.sortie())
+    for k in dico:
+        # print(k)
+        for entree in dico[k]:
+            s = str(entree['annee'])
+            s += " "
+            s += entree['type']
+            s += " "
+            s += entree['prenom']
+            s += " "
+            s += entree['nom']
+            #
+            print(s)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='motd',
+    version='0.1',
+    description='Mathematicians Of The Day\nhttp://mathshistory.st-andrews.ac.uk/Day_files/Year.html\nNot an official package',
+    url='https://twitter.com/david_cobac',
+    author='David COBAC',
+    author_email='david.cobac@gmail.com',
+    license='CC-BY-NC-SA',
+    packages=find_packages()
+)
